8bf0cdf55f17c02059da474a24079cda5c6f8ae6
NazmulAlamShawon <shawonkst15@gmail.com>
2025-05-29 10:24:07 +0600

    sign in page setup

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return(
+        <div>
+        <h2> sign in </h2>
+       </div>
+    )
+}
+
+export default Page;
---
+++ src/app/sign-out/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return(
+        <div>
+        <h2> sign out </h2>
+       </div>
+    )
+}
+
+export default Page;
